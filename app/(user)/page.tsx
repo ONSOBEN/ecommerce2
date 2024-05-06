55f3cf9ff77e548baa12ebe47059932408112eec
ben <sobenkh35@gmail.com>
2024-05-06 23:31:17 +0700

    my shop

--- app/(user)/page.tsx
+++ app/(user)/page.tsx
@@ -3,7 +3,15 @@
 import React from 'react';
 import { GlobeDemo } from '@/components/ui/globe_Demo';
 import Products from '@/components/product/products';
-
+import { Metadata } from 'next';
+export const metadata: Metadata = {
+    title: "final Project ",
+    description: "This is home page",
+    openGraph:{
+        images:"https://media.istockphoto.com/id/1361979526/vector/map-pin-place-marker-location-vector-icon-pin-point-icon-vector-logo-destination-logo-online.jpg?s=612x612&w=0&k=20&c=yzhK0497GmC9efCCGb02go0uj_HiUMQEtmDduY9Qmzs=",
+        description:"Home page for mini project",
+    }
+  };
 
 export default function Home() {
 	return (
